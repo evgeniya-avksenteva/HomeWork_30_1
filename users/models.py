1322c5b26e7590e9c242f81c4a819a6ebe64e783
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-08-07 16:54:07 +0300

    initial commit

---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Укажите почту"
+    )
+
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите свой аватар",
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Введите название города",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
